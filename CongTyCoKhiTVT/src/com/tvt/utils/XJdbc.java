0b34bfcb5fcf54b46f1cd8ec3b8bee49d539751f
phu-haha <xuanphuff123@gmail.com>
2025-03-28 13:51:33 +0700

    Create .gitignore

--- CongTyCoKhiTVT/src/com/tvt/utils/XJdbc.java
+++ CongTyCoKhiTVT/src/com/tvt/utils/XJdbc.java
@@ -16,9 +16,9 @@ import java.sql.SQLException;
  */
 public class XJdbc {
     private static String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    private static String dburl="jdbc:sqlserver://localhost;database=EduSys;encrypt=true;trustServerCertificate=true;";
-    private static String username="Admin_user";
-    private static String password="123";
+    private static String dburl="jdbc:sqlserver://localhost;database=Congtytvt;encrypt=true;trustServerCertificate=true;";
+    private static String username="sa";
+    private static String password="21092007";
    
     static{
         try {            
